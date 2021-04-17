378a708e670995025bc8612dd3cccf6cd7e353dd
MuHammadYousry <60557082+MuHammadYousry@users.noreply.github.com>
2021-04-17 14:51:45 +0200

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -14,7 +14,7 @@ curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercont
 curl -o "C:\Users\Public\Desktop\npp.7.9.4.Installer.x64.exe" https://raw.githubusercontent.com/MuHammadYousry/RDP_Windows2019/main/Files/npp.7.9.4.Installer.x64.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Everything.exe" https://raw.githubusercontent.com/MuHammadYousry/RDP_Windows2019/main/Files/Everything.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\BANDIZIP-SETUP.exe" https://raw.githubusercontent.com/MuHammadYousry/RDP_Windows2019/main/Files/BANDIZIP-SETUP.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Setup RenameMaster.exe" https://raw.githubusercontent.com/MuHammadYousry/RDP_Windows2019/main/Files/Setup RenameMaster.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Rename Master ZIP.zip" https://raw.githubusercontent.com/MuHammadYousry/RDP_Windows2019/main/Files/Rename Master ZIP.zip > out.txt 2>&1
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
